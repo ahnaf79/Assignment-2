f32c4e5ef9a3b4b9334040627a230b9733a849d9
ahnaf79 <ahnaf.gdis2000@gmail.com>
2022-07-03 10:51:39 +0600

    create contact.js

---
+++ routes/contact.js
@@ -0,0 +1,29 @@
+/* File name: contact.js
+Student name: Ahnaf Tahmid
+StudentID: 301221126
+Date: 1st July 2022 */
+
+let express = require('express');
+let router = express.Router();
+let contactController = require('../controllers/contactList');
+
+function requireAuth(req, res, next) {
+    if (!req.isAuthenticated() {
+            return res.redirect('../../users/login');
+        }
+        next();)
+}
+
+//GET contact list
+router.get('/', contactController.displayContactList);
+
+//GET particular contact info
+router.get('/edit/:id', requireAuth, contactController.displayEditPage);
+
+//POST particular contact info
+router.post('/edit/:id', requireAuth, contactController.processEditPage);
+
+//GET to delete particaular contact info
+router.get('/delete/:id', requireAuth, contactController.performDelete);
+
+module.exports = router;
