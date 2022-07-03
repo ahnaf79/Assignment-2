5bc531772e6e9f3d3e46bc79a15ac09051b5b66e
ahnaf79 <ahnaf.gdis2000@gmail.com>
2022-07-03 09:36:42 +0600

    creating db,js

---
+++ config/db.js
@@ -0,0 +1,9 @@
+/* File name: db.js
+Student name: Ahnaf Tahmid
+StudentID: 301221126
+Date: 1st July 2022 */
+
+module.exports = {
+    URI: "mongodb+srv://ahnaf79:<password>@assignment2.do6le.mongodb.net/?retryWrites=true&w=majority",
+    Secret: "SomeSecret"
+};
